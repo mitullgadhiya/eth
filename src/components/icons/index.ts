751aeacd731dab73c9257cab3be03d31fef65527
mitulgadhiya <88765850+mitullgadhiya@users.noreply.github.com>
2025-02-25 21:33:30 +0530

    Add files via upload

---
+++ src/components/icons/index.ts
@@ -0,0 +1,20 @@
+// General Icons
+import CreatorFeeImage from "./creator-fee.svg"
+import EthHomeIcon from "./eth-home-icon.svg"
+import { HighlightDarkIcon } from "./HighlightDarkIcon"
+import { HighlightIcon } from "./HighlightIcon"
+import LiquidityImage from "./liquidity-share.svg"
+import MarketingImage from "./marketing-share.svg"
+import PresaleImage from "./presale-share.svg"
+import RamiHomeIcon from "./rami-home-icon.svg"
+
+export {
+  CreatorFeeImage,
+  EthHomeIcon,
+  HighlightDarkIcon,
+  HighlightIcon,
+  LiquidityImage,
+  MarketingImage,
+  PresaleImage,
+  RamiHomeIcon,
+}
