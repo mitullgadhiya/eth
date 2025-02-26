c45553427f2d080b63d93b965db6d970644c00c7
mitulgadhiya <88765850+mitullgadhiya@users.noreply.github.com>
2025-02-27 02:23:56 +0530

    p

--- app/swap/page.tsx
+++ app/swap/page.tsx
@@ -1,45 +1,7 @@
-import MySwapBox from "@/components/DJ/MySwapBox"
-import MainArticle from "@/components/MainArticle"
-import PageHero from "@/components/PageHero"
-import { Divider } from "@/components/ui/divider"
+import SwapPage from "@/components/SwapPage";
 
-import HeroImage from "../../public/images/wallets/wallet-hero.png"
-
-const Home = () => {
-
-  const heroContent = {
-    title: "Ramicoin Exchange",
-    header: "Trade Digital Assets",
-    subtitle: "Buy what matters than just buying a trend.",
-    image: HeroImage,
-    alt: "Illustration of a person swapping digital assets, representing Ramicoin Exchange",
-    // TODO: remove conditional after soft launch
-    buttons:
-      [
-        {
-          href: "/swap/",
-          content: "Presale",
-        },
-        {
-          href: "/swap/",
-          content: "$0.005",
-          variant: "outline" as const,
-        },
-      ]
-
-  }
-
+export default function Home() {
   return (
-    <MainArticle className="mx-auto flex w-full flex-col items-center">
-      <PageHero content={heroContent} isReverse />
-
-      <div className="w-full px-8 py-4">
-        <Divider />
-        <MySwapBox />
-        <Divider />
-      </div>
-    </MainArticle>
+    <SwapPage />
   )
 }
-
-export default Home
