50aedef959a86f50de97a5d5b23377c38da4d14a
mitulgadhiya <88765850+mitullgadhiya@users.noreply.github.com>
2025-02-27 01:37:38 +0530

    c

---
+++ next.config.mjs
@@ -0,0 +1,57 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    // SVG loader
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+  trailingSlash: true,
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1504, 1920],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "crowdin-static.downloads.crowdin.com",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply to all routes
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
