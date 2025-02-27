c955847a38d88dde3eff4771cb7a8df7289b73e0
mitulgadhiya <88765850+mitullgadhiya@users.noreply.github.com>
2025-02-27 16:40:00 +0530

    Add files via upload

---
+++ config/index.tsx
@@ -0,0 +1,29 @@
+'use lient'
+
+// config/index.tsx
+
+import { cookieStorage, createStorage, http } from '@wagmi/core'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import { bsc, mainnet } from '@reown/appkit/networks'
+import type { AppKitNetwork } from '@reown/appkit/networks'
+
+// Get projectId from https://cloud.reown.com
+export const projectId = '11ce127cd437eec40ae569df49b8f4bb'
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+export const networks = [bsc, mainnet] as [AppKitNetwork, ...AppKitNetwork[]]
+
+//Set up the Wagmi Adapter (Config)
+export const wagmiAdapter = new WagmiAdapter({
+  storage: createStorage({
+    storage: cookieStorage
+  }),
+  ssr: true,
+  projectId,
+  networks
+})
+
+export const config = wagmiAdapter.wagmiConfig
